06c4c18287c5506ac52cafddba2b92bda217351e
Andrey Azov <andrey.azov@gmail.com>
2018-12-04 01:25:31 +0300

    Add ramda

--- challenges/day-2/day-2-solution.js
+++ challenges/day-2/day-2-solution.js
@@ -8,13 +8,13 @@ import { readInput } from './day-2-helpers';
 function solveFirstPuzzle() {
   const input = readInput();
   const solution = calculateChecksum(input);
-  console.log('Solution to the first part of day 1 is', solution);
+  console.log('Solution to the first part of day 2 is', solution);
 }
 
 function solveSecondPuzzle() {
   const input = readInput();
   const solution = findSameLettersOfClosestStrings(input);
-  console.log('Solution to the second part of day 1 is', solution);
+  console.log('Solution to the second part of day 2 is', solution);
 }
 
 solveFirstPuzzle();
